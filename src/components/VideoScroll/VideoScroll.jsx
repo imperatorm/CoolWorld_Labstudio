458281fc074ed04f97db39274f09aab35424a977
LadyDev54 <>
2023-07-04 12:14:33 +0400

    video

--- src/components/VideoScroll/VideoScroll.jsx
+++ src/components/VideoScroll/VideoScroll.jsx
@@ -10,6 +10,7 @@ import styles from "./VideoScroll.module.scss";
 import { WorldIcon } from "../Icons/Icons";
 const VideoScroll = () => {
   const maskRef = useRef(null);
+  const videoRef = useRef(null);
   const elementRef = useRef(null)
 
   // useEffect(() => {
@@ -33,45 +34,64 @@ const VideoScroll = () => {
     useEffect(() => {
       const maskEl = maskRef.current;
       if (maskEl) {
-        gsap.fromTo(
-          maskEl,
-          { maskSize: "20%" },
-          {
-            maskSize: "100%",
-            duration: 3,
-           
-            scrollTrigger: {
-              trigger: elementRef.current,
-              start: 800,
-              end: "bottom 50%",
-              scrub: true,
-              toggleActions: "restart none none none",
-              // markers: true,
-            },
-          }
-        );
-        gsap.to(".xoski", {
+        const tl = gsap.timeline({
           scrollTrigger: {
-            trigger: ".xoski",
-            scrub: true,
+            trigger: maskEl,
+            start: "-50% 30%",
             pin: true,
-            start: "center center",
-            end: "bottom -100%",
-            toggleClass: "active",
-            ease: "power2",
+            // pinSpacing:false,
+            scrub: 1,
+            toggleActions: "restart reverse none none",
+            // play pause resume reverse restart reset complete none
+            // onEnter onLeave onEnterBack onLeaveBack
+            markers: true,
           },
         });
+        tl.to(maskEl, { maskSize: "100%", duration: 3 })
+          .to(maskEl, { opacity: "0", duration: 2 })
+          // .to(videoRef, { zIndex:1, duration: 2 });
+        // gsap.fromTo(
+        //   maskEl,
+        //   { maskSize: "20%" },
+        //   {
+        //     maskSize: "100%",
+        //     duration: 3,
+
+        //     scrollTrigger: {
+        //       trigger: maskEl,
+        //       start: "-50% 30%",
+        //       pin: true,
+        //       // pinSpacing:false,
+        //       scrub: 4,
+        //       toggleActions: "restart reverse none none",
+        //       // play pause resume reverse restart reset complete none
+        //       // onEnter onLeave onEnterBack onLeaveBack
+        //       markers: true,
+        //     },
+        //   }
+        // );
+        // gsap.to("#xoski", {
+        //   scrollTrigger: {
+        //     trigger: "#xoski",
+        //     scrub: true,
+        //     pin: true,
+        //     start: "top center",
+        //     end: "bottom -100%",
+        //   },
+        // });
       }
     }, [maskRef]);
   return (
-    <div id="xoski">
+    <div id="xoski" ref={elementRef}>
       <div className={styles.mask} ref={maskRef}>
         <video className={styles.video} autoPlay muted loop>
           <source src={"./111.mp4"} type="video/mp4" />
         </video>
       </div>
+      <video ref={videoRef} className={styles.video2} autoPlay muted loop>
+        <source src={"./111.mp4"} type="video/mp4" />
+      </video>
 
-      <div ref={elementRef}></div>
       {/* <WorldIcon /> */}
     </div>
   );
