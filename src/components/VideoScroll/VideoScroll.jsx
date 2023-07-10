37a8eb487e0aa4e0ce11d90d02b4a59e5fecf2d0
LadyDev54 <>
2023-07-10 10:52:14 +0400

    video scroll

--- src/components/VideoScroll/VideoScroll.jsx
+++ src/components/VideoScroll/VideoScroll.jsx
@@ -11,29 +11,41 @@ const VideoScroll = () => {
   const elementRef = useRef(null)
   useEffect(() => {
       const maskEl = maskRef.current;
+      
       if (maskEl) {
         const tl = gsap.timeline({
           scrollTrigger: {
-            trigger: maskEl,
-            start: "center center",
+            trigger: elementRef.current,
+            // start: "center center",
             pin: true,
             // pinSpacing:false,
             scrub: 1,
+            start: "top top",
+            end: "+=3000 bottom",
             toggleActions: "restart reverse none none",
             // play pause resume reverse restart reset complete none
             // onEnter onLeave onEnterBack onLeaveBack
-            // markers: true,
+            markers: true,
           },
         });
         tl.fromTo(
           maskEl,
           { maskImage: `url(${maskImg.src})` },
-          { maskSize: "100%", duration: 3 }
+          { maskSize: "100%", duration: 1 }
         )
+          // tl.add(maskInitial)
+          // tl.fromTo(
+          //   maskEl,
+          //   { maskImage: `url(${maskImg.src})` },
+          //   { maskSize: "100%", duration: 2 }
+          // )
+          // .to(maskEl, { maskSize: "100%", duration: 1 })
           .to(maskEl, {
-            maskImage: "none !important",
-            duration: 1,
+            maskImage: "none",
+            // duration: 1,
           })
+          // .set(maskEl, { maskImage: "none !important" })
+
           .to("#videoBannerText", {
             opacity: "1",
             transform: "translate(50%, 50%)",
@@ -46,9 +58,8 @@ const VideoScroll = () => {
       }
     }, [maskRef]);
   return (
-    <div>
-      <div id="xoski" ref={elementRef}>
-        <div className={styles.mask} ref={maskRef}  >
+      <div id="videoWrap" className={styles.videoWrap} ref={elementRef}>
+        <div className={styles.mask} ref={maskRef}  style={{maskImage: `url(${maskImg.src})`}}>
           {/* <Image src={worldImg} /> */}
           <video className={styles.video} autoPlay muted loop>
             <source src={"./111.mp4"} type="video/mp4" />
@@ -57,14 +68,13 @@ const VideoScroll = () => {
             ohne nass zu werden
           </h3>
         </div>
-        <div className={`${styles.paraContent} `}>
+        {/* <div className={`${styles.paraContent} `}>
           <h2>
             Erfrischende Kühlzonen für heiße Tage: Genieße den Sommer mit
             unseren Cooling Oasis
           </h2>
-        </div>
+        </div> */}
       </div>
-    </div>
   );
 };
 
